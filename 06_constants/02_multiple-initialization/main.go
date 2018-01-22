b28fe510e5b6dff135a1b3902539d7300ec97aff
Alexander Ivanov <ivanov_aa@bifit.com>
2018-01-22 16:45:50 +0300

    constants

---
+++ 06_constants/02_multiple-initialization/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	Pi       = 3.14
+	Language = "Golang"
+)
+
+func main() {
+	fmt.Println(Pi)
+	fmt.Println(Language)
+}
---
+++ 06_constants/04_iota/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const (
+	_ = iota
+	B = iota * 10
+	C = iota * 10
+)
+
+func main() {
+	fmt.Println(B)
+	fmt.Println(C)
+}
